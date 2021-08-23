f8b1760e1f00202eb7d2d1a5bce95c7f4d087b20
Berserker <nizamudeen5619@gmail.com>
2021-08-23 16:32:58 +0530

    Update user-router.js

--- src/routers/user-router.js
+++ src/routers/user-router.js
@@ -17,7 +17,6 @@ router.post('/users', async (req, res) => {
         res.status(201).send({ user, token })
     }
     catch (error) {
-        console.log(error)
         res.status(400).send(error)
     }
 })
