ec60ea06d8c170ba6c5501d5bc302f950abb460d
Berserker <nizamudeen5619@gmail.com>
2021-10-14 10:21:09 +0530

    Added title to task-model

--- src/models/task-model.js
+++ src/models/task-model.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose')
 
 const taskSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        trim:true,
+        required:true
+    },
     description: {
         type: String,
         trim: true,
